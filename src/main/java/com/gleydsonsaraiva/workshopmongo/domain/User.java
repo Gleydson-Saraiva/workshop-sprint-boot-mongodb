121dcdf487a0663f026d45d535d314d948a389a5
Gleydson-Saraiva <gleydson.icm@gmail.com>
2024-12-23 09:03:41 -0300

    Conectando ao MongoDB com repository e service e foi corrigido o código das class - UserRepository.java

--- src/main/java/com/gleydsonsaraiva/workshopmongo/domain/User.java
+++ src/main/java/com/gleydsonsaraiva/workshopmongo/domain/User.java
@@ -1,12 +1,17 @@
 package com.gleydsonsaraiva.workshopmongo.domain;
 
 import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 //import java.util.Objects;
 
+@Document(collection="user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
@@ -48,7 +53,10 @@ public class User implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime + result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -60,6 +68,11 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
---
+++ src/main/java/com/gleydsonsaraiva/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gleydsonsaraiva.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gleydsonsaraiva.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
